b788f58a2a749d3137b6fd66190d3fe4fb82c676
RachData <rachidderra@gmail.com>
2020-03-28 17:40:29 +0100

    Copies des classe de l'exercice 4.1

---
+++ Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/Iterator.java
@@ -0,0 +1,8 @@
+package fr.uvsq.pglp.Exercice5_1;
+
+public interface Iterator {
+	
+	boolean HasNext();
+	PersonelsInter Next();
+
+}
---
+++ Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/PersonelsInter.java
@@ -0,0 +1,9 @@
+package fr.uvsq.pglp.Exercice5_1;
+
+import java.io.Serializable;
+
+public interface PersonelsInter extends Serializable {
+	
+	void print();
+
+}
