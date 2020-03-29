34e9adee2bf8e48ca480a6c1693cc0d9f05be40b
RachData <rachidderra@gmail.com>
2020-03-29 03:23:48 +0200

    Creation de la classe abstraite DAO

---
+++ Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/DAO.java
@@ -0,0 +1,13 @@
+package fr.uvsq.pglp.Exercice5_1;
+
+import java.sql.Connection;
+
+public abstract class DAO<T> {
+	
+	protected Connection connect ;
+	public abstract T create(T obj);
+	public abstract T find(String id);
+	public abstract T update( T obj);
+	public abstract void delete (T obj);
+
+}
