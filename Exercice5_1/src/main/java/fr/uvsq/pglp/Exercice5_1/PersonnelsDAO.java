3f63077a11f946375acf50b88ee75b7b3bcd911d
RachData <rachidderra@gmail.com>
2020-03-29 03:31:18 +0200

    Creation de la classe Personnels DAO

---
+++ Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/PersonnelsDAO.java
@@ -0,0 +1,29 @@
+package fr.uvsq.pglp.Exercice5_1;
+
+public class PersonnelsDAO extends DAO<Personnels>{
+
+	@Override
+	public Personnels create(Personnels obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personnels find(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personnels update(Personnels obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Personnels obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
