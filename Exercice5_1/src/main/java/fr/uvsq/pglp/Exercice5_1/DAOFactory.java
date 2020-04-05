cf6c8a18d2a79b801991432c4711f14079815b48
RachData <rachidderra@gmail.com>
2020-04-05 02:49:39 +0200

    Ajoout de la factory CompositePersonells

--- Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/DAOFactory.java
+++ Exercice5_1/src/main/java/fr/uvsq/pglp/Exercice5_1/DAOFactory.java
@@ -1,9 +1,17 @@
 package fr.uvsq.pglp.Exercice5_1;
 
+import java.sql.Connection;
+
+
 public class DAOFactory {
 	
-	public static DAO<Personnels> getPersonneDAO(){
+	public static DAO<Personnels> PersonnelsDAO(Connection conn){
 		return new PersonnelsDAO();
 	}
+	
+	public static DAO<CompositePerso> getCompositePerso(Connection conn){
+		return new CompositePersoDAO();
+	}
+
 
 }
